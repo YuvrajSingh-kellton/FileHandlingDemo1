3ac088bff7395f76eb3103e4d9051799690a6e42
YuvrajSingh-kellton <105622006+YuvrajSingh-kellton@users.noreply.github.com>
2022-08-10 11:03:56 +0530

    Initial Commit

---
+++ FileHandleDemo/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace OOPSConsoleDemo
+{
+    public class FileHand
+    {
+        public void WriteFile()
+        {
+            FileStream fileStreamObj = new FileStream(@"D:\CSprogram\User.txt", FileMode.Create, FileAccess.Write);
+            StreamWriter streamWriterObj = new StreamWriter(fileStreamObj);
+            streamWriterObj.Close();
+            fileStreamObj.Close();
+            Console.WriteLine("File write operation completed");
+
+        }
+
+        public void ReadFile()
+        {
+            FileStream File = new FileStream(@"D:\CSprogram\User.txt", FileMode.Open, FileAccess.ReadWrite);
+            StreamReader Stream = new StreamReader(File);
+            StreamWriter Stream2 = new StreamWriter(File);
+
+            Console.WriteLine("StudId\tName\tMarks\tAge");
+            int i,StudId,Marks,Age;
+            string name;
+            Console.WriteLine("How Many records You Want to Enter");
+            int n = Console.Read();
+            for(i = 0; i < n; i++)
+            {
+                Console.WriteLine("Enter Stud ID");
+                int value = Console.Read();
+                int Id = Convert.ToInt32(value);
+                Console.Write("\t");
+
+            }
+
+            Stream.ReadLine();
+            
+            while (Stream.Peek() > 0)
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ FileHandleDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace OOPSConsoleDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileHand Call = new FileHand();
+           // Call.WriteFile();
+            Call.ReadFile();
+        }
+    }
+}
